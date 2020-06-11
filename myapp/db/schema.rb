94f55bcd71138856da61cc3382e174af79f3917f
github username <github email address>
2020-06-11 15:04:16 -0700

    updated db

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_204242) do
+ActiveRecord::Schema.define(version: 2020_06_11_220335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 2020_06_11_204242) do
     t.integer "neighborhood_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
   create_table "zipcode_neighborhoods", force: :cascade do |t|
