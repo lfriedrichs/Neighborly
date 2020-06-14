0acdd8b67661f9dc4ebd1b4ed3b676705fbcdece
github username <github email address>
2020-06-13 22:45:10 -0700

    backend testing 1

--- myapp/app/controllers/api/v1/asks_controller.rb
+++ myapp/app/controllers/api/v1/asks_controller.rb
@@ -17,6 +17,19 @@ class AsksController < ApplicationController
       end
     end
 
+    def update
+      ask = Ask.find(params[:ask][:id])
+      offer_id = params[:ask][:offer_id]
+      ask.offer_id = offer_id
+      offer = Offer.find(offer_id)
+      offer.ask = ask
+      if ask.valid? && offer.valid?
+        render json: { message: 'offer selected'}, status: :accepted
+      else
+        render json: { error: 'failed to select offer' }, status: :not_acceptable
+      end
+    end
+
     
       private
       def ask_params
--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :update]
-      resources :asks, only: [:create, :delete]
+      resources :asks, only: [:create, :delete, :update]
       resources :offers, only: [:create, :delete]
       resources :neighborhoods, only: [:show, :update]
       post '/login', to: 'auth#create'
