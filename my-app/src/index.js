6a7de519809d531d9ad692f26788586721fabb39
github username <github email address>
2020-06-15 14:33:15 -0700

    inserted Router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import manageUser from './reducers/manageUser'
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
 const store = createStore(manageUser)
 
 ReactDOM.render(
+
   <Provider store={store}>
-    <App />
+    <Router >
+      <Route exact path="/" component={App} />
+    </Router>),
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- my-app/src/reducers/manageUser.js
+++ my-app/src/reducers/manageUser.js
@@ -1,19 +1,22 @@
-let user = {}
-export default function manageUser(state = user, action) {
+
+export default function manageUser(state = {user: {asks: [""], offers: [""]}}, action) {
     switch(action.type) {
         case "UPDATE_USER":
             return {...state, user: action.user}
         case "ADD_ASK":
             return {...state, user: {...state.user, asks: [...state.asks, action.ask]}}
         case "DELETE_ASK":
-            return {...state, user: {...state.user, asks: [state.asks.filter(ask => {ask.id !== action.askId})]}}
+            let asks = state.user.asks.filter(ask => ask.id !== action.askId)
+            return {...state, user: {...state.user, asks: asks}}
         case "SELECT_OFFER_FOR_ASK":
-            ask = state.user.asks.filter(ask => {ask.id === action.askId}).offerId = action.offerId;
+            let ask = state.user.asks.filter(ask => ask.id === action.askId)
+            ask.offerId = action.offerId;
             return {...state, user: {...state.user, asks: [...state.asks, ask]}}
         case "ADD_OFFER":
             return {...state, user: {...state.user, offers: [...state.offers, action.offer]}}
         case "DELETE_OFFER":
-            return {...state, user: {...state.user, offers: [state.offers.filter(offer => {offer.id !== action.askId})]}}
+            let offers = state.offers.filter(offer => offer.id !== action.askId)
+            return {...state, user: {...state.user, offers: offers}}
         default:
             return state
     }
